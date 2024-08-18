3c624e3a2074f5654bdd9c51d62681216534a007
jhums04 <jbonganciso04@gmail.com>
2024-08-18 21:19:51 +0800

    added cors mapping

---
+++ src/main/java/com/fullstack_app_with_react_ts/fullstacknoteapp/config/WebConfig.java
@@ -0,0 +1,17 @@
+package com.fullstack_app_with_react_ts.fullstacknoteapp.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/**")
+                .allowedOrigins("http://localhost")
+                .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
+                .allowedHeaders("*");
+    }
+}
