b54bccbb6b9eeea597776145e15dbe8298a307ee
jhums04 <jbonganciso04@gmail.com>
2024-08-18 22:13:08 +0800

    Added following: -- added localhost to cater nginx react on docker

--- src/main/java/com/fullstack_app_with_react_ts/fullstacknoteapp/config/WebConfig.java
+++ src/main/java/com/fullstack_app_with_react_ts/fullstacknoteapp/config/WebConfig.java
@@ -10,7 +10,7 @@ public class WebConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3001")
+                .allowedOrigins("http://localhost:3001", "http://localhost")
                 .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
                 .allowedHeaders("*");
     }
